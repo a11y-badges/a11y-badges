b5fbfb0a25c7ebf2c3fdbb375fbd51af969c3129
ctcpip <ctcpip@users.noreply.github.com>
2022-12-02 00:56:45 -0600

    ✅ use visual regression for tests instead of svg text comparison

--- spec/badge-spec.js
+++ spec/badge-spec.js
@@ -1,51 +1,64 @@
 const { getBadge } = require('../badge/generator');
-const fs = require('fs');
 const b64Logos = require('./badge-refs/base64-logos');
+const looksSame = require('looks-same');
 
 const dataImagePrefix = 'data:image/svg+xml;base64,';
 
 describe('the badges', () => {
 
-  it('renders badges correctly', () => {
+  it('renders badges correctly', async() => {
 
-    const b1 = getBadge({ logo: 'ibm' });
-    expect(b1).withContext('simple icon badge is correct').toBe(fs.readFileSync('./spec/badge-refs/ibm.svg', 'utf8'));
+    let b, samesame;
 
-    const b2 = getBadge({ logo: 'check-square', text: 'a11y badges', badgeColor: 'green' });
-    expect(b2).withContext('feather icon badge is correct').toBe(fs.readFileSync('./spec/badge-refs/a11y-badges.svg', 'utf8'));
+    b = getBadge({ logo: 'ibm' });
+    samesame = (await looksSame(Buffer.from(b), './spec/badge-refs/ibm.svg')).equal;
+    expect(samesame).withContext('simple icon badge is correct').toBeTrue();
 
-    const b3 = getBadge({ logo: 'github', logoColor: 'skyblue' });
-    expect(b3).withContext('html color badge is correct').toBe(fs.readFileSync('./spec/badge-refs/github.svg', 'utf8'));
+    b = getBadge({ logo: 'check-square', text: 'a11y badges', badgeColor: 'green' });
+    samesame = (await looksSame(Buffer.from(b), './spec/badge-refs/a11y-badges.svg')).equal;
+    expect(samesame).withContext('feather icon badge is correct').toBeTrue();
 
-    const b4 = getBadge({ text: 'Liverpool FC is the best football club in the world!', badgeColor: 'C8102E' });
-    expect(b4).withContext('hex color badge is correct').toBe(fs.readFileSync('./spec/badge-refs/liverpool-fc.svg', 'utf8'));
+    b = getBadge({ logo: 'github', logoColor: 'skyblue' });
+    samesame = (await looksSame(Buffer.from(b), './spec/badge-refs/github.svg')).equal;
+    expect(samesame).withContext('html color badge is correct').toBeTrue();
 
-    const b5 = getBadge({ badgeColor: 'rgb(0,255,0)' });
-    expect(b5).withContext('rgb color badge is correct').toBe(fs.readFileSync('./spec/badge-refs/rgb.svg', 'utf8'));
+    b = getBadge({ text: 'Liverpool FC is the best football club in the world!', badgeColor: 'C8102E' });
+    samesame = (await looksSame(Buffer.from(b), './spec/badge-refs/liverpool-fc.svg')).equal;
+    expect(samesame).withContext('hex color badge is correct').toBeTrue();
 
-    const b6 = getBadge({ badgeColor: 'rgba(127,212,190,0.46)' });
-    expect(b6).withContext('rgba color badge is correct').toBe(fs.readFileSync('./spec/badge-refs/rgba.svg', 'utf8'));
+    b = getBadge({ badgeColor: 'rgb(0,255,0)' });
+    samesame = (await looksSame(Buffer.from(b), './spec/badge-refs/rgb.svg')).equal;
+    expect(samesame).withContext('rgb color badge is correct').toBeTrue();
 
-    const b7 = getBadge({ logo: dataImagePrefix + b64Logos.afterburner });
-    expect(b7).withContext('afterburner badge is correct').toBe(fs.readFileSync('./spec/badge-refs/afterburner.svg', 'utf8'));
+    b = getBadge({ badgeColor: 'rgba(127,212,190,0.46)' });
+    samesame = (await looksSame(Buffer.from(b), './spec/badge-refs/rgba.svg')).equal;
+    expect(samesame).withContext('rgba color badge is correct').toBeTrue();
 
-    const b8 = getBadge({ logo: dataImagePrefix + b64Logos.openjsf });
-    expect(b8).withContext('openjsf badge is correct').toBe(fs.readFileSync('./spec/badge-refs/openjsf.svg', 'utf8'));
+    b = getBadge({ logo: dataImagePrefix + b64Logos.afterburner });
+    samesame = (await looksSame(Buffer.from(b), './spec/badge-refs/afterburner.svg')).equal;
+    expect(samesame).withContext('afterburner badge is correct').toBeTrue();
 
-    const b9 = getBadge({ logo: dataImagePrefix + b64Logos.tc39 });
-    expect(b9).withContext('tc39 badge is correct').toBe(fs.readFileSync('./spec/badge-refs/tc39.svg', 'utf8'));
+    b = getBadge({ logo: dataImagePrefix + b64Logos.openjsf });
+    samesame = (await looksSame(Buffer.from(b), './spec/badge-refs/openjsf.svg')).equal;
+    expect(samesame).withContext('openjsf badge is correct').toBeTrue();
 
-    const b10 = getBadge({ logo: dataImagePrefix + b64Logos.whatwg });
-    expect(b10).withContext('whatwg badge is correct').toBe(fs.readFileSync('./spec/badge-refs/whatwg.svg', 'utf8'));
+    b = getBadge({ logo: dataImagePrefix + b64Logos.tc39 });
+    samesame = (await looksSame(Buffer.from(b), './spec/badge-refs/tc39.svg')).equal;
+    expect(samesame).withContext('tc39 badge is correct').toBeTrue();
 
-    const b11 = getBadge({
+    b = getBadge({ logo: dataImagePrefix + b64Logos.whatwg });
+    samesame = (await looksSame(Buffer.from(b), './spec/badge-refs/whatwg.svg')).equal;
+    expect(samesame).withContext('whatwg badge is correct').toBeTrue();
+
+    b = getBadge({
       badgeColor: 'darkgreen',
       logo: dataImagePrefix + b64Logos.afterburner,
       logoColor: 'lightblue',
       text: 'afterburner js',
       textColor: 'lightgrey',
     });
-    expect(b11).withContext('all fields badge is correct').toBe(fs.readFileSync('./spec/badge-refs/all-the-fields.svg', 'utf8'));
+    samesame = (await looksSame(Buffer.from(b), './spec/badge-refs/all-the-fields.svg')).equal;
+    expect(samesame).withContext('all fields badge is correct').toBeTrue();
 
   });
 
